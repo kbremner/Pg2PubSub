6f91157952d18f14be31881bf85274238a69fa43
Kyle Bremner <contact@kbremner.com>
2016-04-07 20:47:39 +0100

    Removes unnecessary GenServer

--- lib/pg2pubsub.ex
+++ lib/pg2pubsub.ex
@@ -2,50 +2,7 @@ defmodule Pg2PubSub do
   @moduledoc """
   Provides methods for subscribing and publishing to named topics.
   """
-
-  use GenServer
   require Logger
-
-  @doc """
-  Starts a PubSub process linked to the calling process
-
-  ## Examples
-
-      iex> {:ok, pid} = Pg2PubSub.start_link
-      iex> is_pid(pid)
-      true
-
-  """
-  @spec start_link() :: GenServer.on_start
-  def start_link() do
-    :ok = Logger.debug "Publisher starting..."
-    GenServer.start_link(__MODULE__, :ok)
-  end
-
-  @doc """
-  Starts a PubSub process linked to the calling process and with the given name
-
-  ## Examples
-
-      iex> {:ok, pid} = Pg2PubSub.start_link :foo
-      iex> is_pid(pid)
-      true
-
-      # can use the name when executing an operation
-      iex> {:ok, pid} = Pg2PubSub.start_link :foo
-      iex> Pg2PubSub.subscribe(:foo, "foo")
-      :ok
-
-  """
-  def start_link(name) do
-    :ok = Logger.debug "Publisher starting with name..."
-    GenServer.start_link(__MODULE__, :ok, name: name)
-  end
-
-  def init(:ok) do
-    :ok = Logger.debug "Publisher started (#{inspect self})"
-    {:ok, self}
-  end
 
   @doc """
   Subscribe to a topic
@@ -57,21 +14,34 @@ defmodule Pg2PubSub do
 
   ## Examples
 
-      iex> {:ok, pid} = Pg2PubSub.start_link
-      iex> Pg2PubSub.subscribe(pid, "foo")
-      :ok
+      iex> Pg2PubSub.subscribe("foo")
+      {:ok, :registered}
 
       # subscribing a second time has no effect
-      iex> {:ok, pid} = Pg2PubSub.start_link
-      iex> Pg2PubSub.subscribe(pid, "foo")
-      :ok
-      iex> Pg2PubSub.subscribe(pid, "foo")
-      {:already_registered, [self]}
+      iex> Pg2PubSub.subscribe("foo")
+      {:ok, :registered}
+      iex> Pg2PubSub.subscribe("foo")
+      {:ok, :already_registered}
 
   """
-  @spec subscribe(pid, String.t) :: term
-  def subscribe(pid, topic) do
-    GenServer.call(pid, {:subscribe, topic, self})
+  @spec subscribe(String.t) :: term
+  def subscribe(topic) do
+    :ok = Logger.debug "#{inspect self} subscribing to #{topic}..."
+    :pg2.create(topic)
+    case :pg2.get_members(topic) do
+      {:error, error} ->
+        :ok = Logger.error "Publisher failed to get members of topic #{topic}: #{error}"
+        {:error, error}
+      pids ->
+        unless self in pids do
+          :pg2.join(topic, self)
+          :ok = Logger.debug "#{inspect self} subscribed to #{topic}"
+          {:ok, :registered}
+      else
+        :ok = Logger.debug "#{inspect self} already subscribed to #{topic}"
+        {:ok, :already_registered}
+      end
+    end
   end
 
 
@@ -85,21 +55,27 @@ defmodule Pg2PubSub do
 
   ## Examples
 
-      iex> {:ok, pid} = Pg2PubSub.start_link
-      iex> Pg2PubSub.subscribe(pid, "foo")
-      :ok
-      iex> Pg2PubSub.unsubscribe(pid, "foo")
+      iex> Pg2PubSub.subscribe("foo")
+      {:ok, :registered}
+      iex> Pg2PubSub.unsubscribe("foo")
       :ok
 
       # unsubscribing when not subscribed will still give an okay result
-      iex> {:ok, pid} = Pg2PubSub.start_link
-      iex> Pg2PubSub.unsubscribe(pid, "foo")
+      iex> Pg2PubSub.unsubscribe("foo")
       :ok
 
   """
-  @spec unsubscribe(pid, String.t) :: term
-  def unsubscribe(pid, topic) do
-    GenServer.call(pid, {:unsubscribe, topic, self})
+  @spec unsubscribe(String.t) :: term
+  def unsubscribe(topic) do
+    :ok = Logger.debug "#{inspect self} unsubscribing from #{topic}..."
+    case :pg2.leave(topic, self) do
+      {:error, {:no_such_group, _topic}} ->
+        :ok = Logger.warn "no subscribers for topic #{topic}"
+        :ok
+      :ok ->
+        :ok = Logger.debug "#{inspect self} unsubscribed from #{topic}"
+        :ok
+    end
   end
 
 
@@ -113,61 +89,22 @@ defmodule Pg2PubSub do
 
   ## Examples
 
-      iex> {:ok, pid} = Pg2PubSub.start_link
-      iex> Pg2PubSub.subscribe(pid, "foo")
-      :ok
-      iex> Pg2PubSub.publish(pid, "foo", "bar")
+      iex> Pg2PubSub.subscribe("foo")
+      {:ok, :registered}
+      iex> Pg2PubSub.publish("foo", "bar")
       :ok
       iex> receive do msg -> msg end
       "bar"
 
   """
-  @spec publish(pid, String.t, any) :: :ok
-  def publish(pid, topic, msg) do
-    GenServer.cast(pid, {:publish, topic, msg})
-  end
-
-  @spec handle_call(term, GenServer.from, term) :: {:reply, :ok, term} | {:stop, term, term}
-  def handle_call({:subscribe, topic, pid}, from = {from_pid, _ref}, s) do
-    :ok = Logger.debug "#{inspect from_pid} subscribing to #{topic}..."
-    :pg2.create(topic)
+  @spec publish(String.t, any) :: :ok
+  def publish(topic, msg) do
     case :pg2.get_members(topic) do
-      {:error, error} ->
-        :ok = Logger.error "Publisher failed to get members of topic #{topic}: #{error}"
-        {:stop, error, s}
+      {:error, err} ->
+        {:error, err}
       pids ->
-        unless pid in pids do
-          :pg2.join(topic, pid)
-          :ok = Logger.debug "#{inspect from_pid} subscribed to #{topic}"
-          {:reply, :ok, s}
-      else
-        :ok = Logger.debug "#{inspect from_pid} already subscribed to #{topic}"
-        GenServer.reply(from, {:already_registered, pids})
-        {:reply, :ok, s}
-      end
-    end
-  end
-
-  def handle_call({:unsubscribe, topic, pid}, from = {from_pid, _ref}, s) do
-    :ok = Logger.debug "#{inspect from_pid} unsubscribing from #{topic}..."
-    case :pg2.leave(topic, pid) do
-      {:error, {:no_such_group, _topic}} ->
-        :ok = Logger.warn "no subscribers for topic #{topic}"
-        {:reply, :ok, s}
-      :ok ->
-        :ok = Logger.debug "#{inspect from_pid} unsubscribed from #{topic}"
-        {:reply, :ok, s}
-    end
-  end
-
-  @spec handle_cast(term, term) :: {:noreply, term}
-  def handle_cast({:publish, topic, message}, s) do
-    case :pg2.get_members(topic) do
-      {:error, _} ->
-        {:noreply, s}
-      pids ->
-        for pid <- pids, do: send(pid, message)
-        {:noreply, s}
+        for pid <- pids, do: send(pid, msg)
+        :ok
     end
   end
 end
--- test/pg2pubsub_test.exs
+++ test/pg2pubsub_test.exs
@@ -9,41 +9,35 @@ defmodule Pg2PubSubTest do
   end
 
   test "won't receive published message if not subscribed" do
-    {:ok, pid} = Pg2PubSub.start_link
-
-    Pg2PubSub.publish(pid, "foo", "bar")
+    Pg2PubSub.publish("foo", "bar")
 
     refute_receive "bar"
   end
 
   test "only receive messages for topics that this process is subscribed to" do
-    {:ok, pid} = Pg2PubSub.start_link
-    Pg2PubSub.subscribe(pid, "bar")
+    Pg2PubSub.subscribe("bar")
 
-    Pg2PubSub.publish(pid, "foo", :foo)
-    Pg2PubSub.publish(pid, "bar", :bar)
+    Pg2PubSub.publish("foo", :foo)
+    Pg2PubSub.publish("bar", :bar)
 
     assert_receive :bar
     refute_receive :foo
   end
 
   test "won't receive messages after unsubscribing from a topic" do
-    {:ok, pid} = Pg2PubSub.start_link
-    Pg2PubSub.subscribe(pid, "foo")
+    Pg2PubSub.subscribe("foo")
 
-    Pg2PubSub.unsubscribe(pid, "foo")
-    Pg2PubSub.publish(pid, "foo", "bar")
+    Pg2PubSub.unsubscribe("foo")
+    Pg2PubSub.publish("foo", "bar")
 
     refute_receive "bar"
   end
 
   test "only receive message once after attempting to subscribe more than once" do
-    {:ok, pid} = Pg2PubSub.start_link
+    Pg2PubSub.subscribe("foo")
+    Pg2PubSub.subscribe("foo")
 
-    Pg2PubSub.subscribe(pid, "foo")
-    Pg2PubSub.subscribe(pid, "foo")
-
-    Pg2PubSub.publish(pid, "foo", :foo)
+    Pg2PubSub.publish("foo", :foo)
 
     assert_receive :foo
     refute_receive :foo # check only received once
